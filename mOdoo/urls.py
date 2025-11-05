00c6edbbe697ae550e67079f0b6662e4b45c8fd5
ipungg-junior <ipungg.id@gmail.com>
2025-11-05 13:45:26 +0700

    remove clear cache url

--- mOdoo/urls.py
+++ mOdoo/urls.py
@@ -1,14 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.core.management import call_command
-from django.core.urlresolvers import clear_url_caches
-from core.models import Module
+from engine.models import Module
 
 def get_dynamic_urlpatterns():
     urlpatterns = [
         path('admin/', admin.site.urls),
-        path('module/', include('core.urls')),
+        path('module/', include('engine.urls')),
     ]
     installed_modules = Module.objects.filter(is_installed=True)
     for module in installed_modules:
