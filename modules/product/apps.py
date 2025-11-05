fffc2e4b6f33cb8eeca59473703ea9b17519fc79
ipungg-junior <ipungg.id@gmail.com>
2025-11-06 00:05:17 +0700

    fix label module

--- modules/product/apps.py
+++ modules/product/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'product'
+    name = 'modules.product'
     label = 'product'
 
     def ready(self):
