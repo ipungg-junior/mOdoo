3400aac38634a31ea84bdfbb988bb68aefdbe9d7
yummypiv.cloud <ipungg.id@gmail.com>
2025-11-07 07:36:25 +0000

    Add new field on product

--- modules/product/models.py
+++ modules/product/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     barcode = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
