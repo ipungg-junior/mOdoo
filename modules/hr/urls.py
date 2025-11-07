1d20c2c402fd5ece2acac7affd4195ad0d1b9532
ipungg-junior <ipungg.id@gmail.com>
2025-11-07 11:28:38 +0700

    add btn to refresh employee table

--- modules/hr/urls.py
+++ modules/hr/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.EmployeeListView.as_view(), name='list_view')
+    path('', views.EmployeeListView.as_view(), name='list_view'),
+    path('sync/', views.SyncEmployeesView.as_view(), name='sync_employees')
 ]
--- modules/hr/views.py
+++ modules/hr/views.py
@@ -3,13 +3,37 @@ from django.views import View
 from .models import Employee
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import User
 
 class EmployeeListView(PermissionRequiredMixin, View):
     permission_required = ['hr.view_employee']
     group_required = ['group_access_hr']
-    
+
     def get(self, request):
         if not request.user.groups.filter(name__in=self.group_required).exists():
             raise PermissionDenied
-        employees = Employee.objects.all()
+        employees = Employee.objects.select_related('user').all()
         return render(request, 'hr_list.html', {'employees': employees})
+
+class SyncEmployeesView(PermissionRequiredMixin, View):
+    permission_required = ['hr.add_employee']
+    group_required = ['group_access_hr']
+
+    def get(self, request):
+        if not request.user.groups.filter(name__in=self.group_required).exists():
+            raise PermissionDenied
+
+        # Sync all users to employees if they don't exist
+        synced_count = 0
+        for user in User.objects.all():
+            employee, created = Employee.objects.get_or_create(
+                user=user,
+                defaults={
+                    'position': 'Employee',
+                    'hire_date': user.date_joined.date()
+                }
+            )
+            if created:
+                synced_count += 1
+
+        return redirect('list_view')
