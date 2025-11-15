7169a0520fec8d649d7e654fc8450f5109a287e7
ipungg-junior <ipungg.id@gmail.com>
2025-11-15 17:00:54 +0700

    fix bug urls.py (product)

--- modules/product/urls.py
+++ modules/product/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Main product management page
     path('', views.ProductPageView.as_view(), name='product_list'),
     # API endpoints for data operations
-    path('api/product/', views.APIView.as_view(), name='product_api', context='product_api'),
-    path('api/category/', views.APIView.as_view(), name='category_api', context='category_api'),
+    path('api/product/', views.APIView.as_view(context='product_api'), name='product_api'),
+    path('api/category/', views.APIView.as_view(context='category_api'), name='category_api'),
 ]
