9dd2bb516d3a8dd389ade62f23ab3b12f6227866
yummypiv.cloud <ipungg.id@gmail.com>
2025-11-15 06:59:59 +0000

    Add new table category product

--- modules/product/models.py
+++ modules/product/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    barcode = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products',
+        null=True,
+        blank=True
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
