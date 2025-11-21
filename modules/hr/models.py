c045c0b5fd66714426ec5dbdc0563403d9d2064a
yummypiv.cloud <ipungg.id@gmail.com>
2025-11-21 06:38:54 +0000

    New table master position employee

--- modules/hr/models.py
+++ modules/hr/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class MasterPosition(models.Model):
+    name = models.CharField(max_length=30, unique=True, null=True, blank=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Employee(models.Model):
-    # models.py
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    position = models.CharField(max_length=100)
+    position = models.ForeignKey(MasterPosition, on_delete=models.CASCADE, null=True, blank=True)
     hire_date = models.DateField()
 
     def __str__(self):
