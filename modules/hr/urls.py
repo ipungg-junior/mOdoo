da1e20f0950eed767e891817627bdfd42b65de1b
ipungg-junior <ipungg.id@gmail.com>
2025-11-26 14:07:49 +0700

    Setup new master position for configuration

--- modules/hr/urls.py
+++ modules/hr/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.EmployeeIndex.as_view(), name='hr_index_page'),
     path('list-employee/', views.EmployeeListPage.as_view(), name='employee_list_page'),
     path('create-employee/', views.EmployeeCreatePage.as_view(), name='employee_create_page'),
+    path('configuration/position/', views.EmployeePositionPage.as_view(), name='employee_position_page'),
     # API endpoints for data operations
     path('api/employee/', views.APIView.as_view(context='employee_api'), name='employee_api')
 ]
