b360bc805001f3cfe224c85184ee487158100b5b
ipungg-junior <ipungg.id@gmail.com>
2025-12-01 14:11:09 +0700

    Add new storage option table

--- engine/models.py
+++ engine/models.py
@@ -6,3 +6,18 @@ class Module(models.Model):
 
     def __str__(self):
         return self.name
+
+class MasterDatabase(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class ModuleMasterDatabase(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    master_database = models.ForeignKey(MasterDatabase, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.module.name} - {self.master_database.name}"
