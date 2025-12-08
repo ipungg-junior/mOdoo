8021c69c198abbd987e5feacb06738f819a152f8
yummypiv.cloud <ipungg.id@gmail.com>
2025-12-08 03:17:38 +0000

    New table for transaction

--- modules/product/models.py
+++ modules/product/models.py
@@ -43,3 +43,27 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Transaction(models.Model):
+    customer_name = models.CharField(max_length=200, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    STATUS_CHOICES = [
+        ('lunas', 'Lunas'),
+        ('belum_lunas', 'Belum Lunas'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='belum_lunas')
+    transaction_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction of {self.product.name} by {self.user.username} on {self.transaction_date}"
+    
+    
+class TransactionItem(models.Model):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='items')
+    product_name = models.CharField(max_length=60)
+    quantity = models.IntegerField()
+    price_per_item = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} at {self.price_per_item} each"
