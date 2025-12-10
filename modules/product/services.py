946ec2af902771b34be57c954e9f6bd4eecd9ece
yummypiv.cloud <ipungg.id@gmail.com>
2025-12-10 20:06:20 +0700

    Add payment term information in table transaction

--- modules/product/services.py
+++ modules/product/services.py
@@ -454,6 +454,7 @@ class TransactionService:
                 'total_price': str(format_rupiah(transaction.total_price)) if transaction.total_price else '0,00',
                 'status': transaction.tmp_status.get_display_name(),
                 'status_value': transaction.tmp_status.name,
+                'payment_term': transaction.payment_term.get_display_name(),
                 'transaction_date': transaction.transaction_date.strftime('%Y-%m-%d %H:%M:%S') if transaction.transaction_date else None,
                 'items': items_data,
                 'items_count': len(items_data)
