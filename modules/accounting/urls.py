e5be27e8e9a891625ae644fbed0b73c786c4c7ae
yummypiv.cloud <ipungg.id@gmail.com>
2025-12-12 00:45:01 +0700

    Update url

--- modules/accounting/urls.py
+++ modules/accounting/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('', views.AccountingPageView.as_view(), name='accounting_index_page'),
     path('report-payable/', views.AccountingPayablePage.as_view(), name='accounting_payable_page'),
     path('report-receivable/', views.AccountingReceivablePage.as_view(), name='accounting_receivable_page'),
-    # API endpoints for data operations
-    path('api/accounting/', views.APIView.as_view(context='accounting_api'), name='accounting_api'),
-    path('api/category/', views.APIView.as_view(context='category_api'), name='category_api'),
 ]
