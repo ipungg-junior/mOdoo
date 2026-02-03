8c3b475576445973eac77d2c1583fec5ba076e82
yummypiv.cloud <ipungg.id@gmail.com>
2026-02-03 14:22:54 +0700

    update format_rupiah() function

--- engine/utils.py
+++ engine/utils.py
@@ -9,21 +9,24 @@ def format_rupiah(amount):
     Returns:
         str: The formatted currency string in Rupiah.
     """
-    try:
-        amount = float(amount)
-    except (ValueError, TypeError):
-        raise ValueError("Invalid amount. Please provide a numeric value.")
+    if amount:
+        try:
+            amount = float(amount)
+        except (ValueError, TypeError):
+            raise ValueError("Invalid amount. Please provide a numeric value.")
 
-    is_negative = amount < 0
-    amount = abs(amount)
+        is_negative = amount < 0
+        amount = abs(amount)
 
-    # Format the number with thousands separator and two decimal places
-    formatted_amount = f"{amount:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+        # Format the number with thousands separator and two decimal places
+        formatted_amount = f"{amount:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
-    if is_negative:
-        return f"{formatted_amount}"
+        if is_negative:
+            return f"{formatted_amount}"
+        else:
+            return f"{formatted_amount}"
     else:
-        return f"{formatted_amount}"
+        return "0,00"
 
 
 # Supabase Storage Service
