c054a256acaef88e11021573693b24b0ea2a249c
yummypiv.cloud <ipungg.id@gmail.com>
2026-04-01 17:13:53 +0700

    changes table field product image (image_path)

--- modules/product/models.py
+++ modules/product/models.py
@@ -46,7 +46,7 @@ class Product(models.Model):
 # Product image model to store multiple images for a product
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image_url = models.CharField(max_length=260, blank=True, null=True, help_text="Storage URL for product image (supabase path)")
+    image_path = models.CharField(max_length=260, blank=True, null=True, help_text="Storage URL for product image (supabase path)")
     signed_url = models.CharField(max_length=750, blank=True, null=True, help_text="Cached signed URL for secure access")
     last_update_signed_url = models.DateTimeField(null=True, blank=True, help_text="Last time signed URL was updated")
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
